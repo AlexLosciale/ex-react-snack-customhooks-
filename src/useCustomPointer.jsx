e5e16f525e232396f9a4e3382d8b67cfa3d324a9
AlexLosciale <losciale17@gmail.com>
2025-05-19 18:51:58 +0200

    add:finsih

--- src/useCustomPointer.jsx
+++ src/useCustomPointer.jsx
@@ -6,3 +6,18 @@
 //Prende in input una stringa o un JSX component (es. un’emoji, un'icona, un'animazione).
 //Posiziona il componente al posto del puntatore del mouse.
 //Il componente segue i movimenti del mouse.
+
+//Esempio di utilizzo:
+
+//*import useCustomPointer from "./useCustomPointer";
+
+//function App() {
+//  const customPointer = useCustomPointer("🔥");
+//  return (
+//    <div>
+//      <h1>Sposta il mouse per vedere il cursore personalizzato!</h1>
+//      {customPointer}
+//    </div>
+//    );
+//}
+//export default App; 
